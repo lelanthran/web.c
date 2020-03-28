8faf2e6658dd7a700ac40f624411594c6bad1a02
lelanthran <lelanthran@gmail.com>
2020-03-28 11:59:26 +0200

    PROGRESS: Added error checking to shell script.

--- daemonize.sh
+++ daemonize.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+if [ -z "$1" ]; then
+   echo You must specify the program to run as a daemon
+   exit 127;
+fi
+
 until $1; do
     echo "Server '$1' crashed with exit code $?.  Respawning..." >&2
     sleep 1
